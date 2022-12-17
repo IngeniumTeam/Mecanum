7916cbfe8764191e328edc199849ab1304cd25f5
Simon Pucheu <104694344+6moon9@users.noreply.github.com>
2022-12-17 10:11:20 +0100

    Update Mecanum.cpp

--- src/Mecanum.cpp
+++ src/Mecanum.cpp
@@ -8,12 +8,12 @@
 Mecanum::Mecanum(   uint8_t in1_1, uint8_t in1_2, uint8_t pwm1, uint8_t offset1,
                     uint8_t in2_1, uint8_t in2_2, uint8_t pwm2, uint8_t offset2, uint8_t standBy1,
                     uint8_t in3_1, uint8_t in3_2, uint8_t pwm3, uint8_t offset3,
-                    uint8_t in4_1, uint8_t in4_2, uint8_t pwm4, uint8_t offset4, uint8_t standBy2)
+                    uint8_t in4_1, uint8_t in4_2, uint8_t pwm4, uint8_t offset4, uint8_t standBy2, int min = 0 int max = 255)
 {
-    motors[Left][Top]       = Motor(in1_1, in1_2, pwm1, offset1, standBy1, 0, 255);
-    motors[Left][Bottom]    = Motor(in2_1, in2_2, pwm2, offset2, standBy1, 0, 255);
-    motors[Right][Top]      = Motor(in3_1, in3_2, pwm3, offset3, standBy2, 0, 255);
-    motors[Right][Bottom]   = Motor(in4_1, in4_2, pwm4, offset4, standBy2, 0, 255);
+    motors[Left][Top]       = Motor(in1_1, in1_2, pwm1, offset1, standBy1, min, max);
+    motors[Left][Bottom]    = Motor(in2_1, in2_2, pwm2, offset2, standBy1, min, max);
+    motors[Right][Top]      = Motor(in3_1, in3_2, pwm3, offset3, standBy2, min, max);
+    motors[Right][Bottom]   = Motor(in4_1, in4_2, pwm4, offset4, standBy2, min, max);
 }
 
 void Mecanum::forward(int speed)

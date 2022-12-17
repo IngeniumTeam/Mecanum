a1855f204d6ca8c511b462653dfb2472886d334d
Simon Pucheu <104694344+6moon9@users.noreply.github.com>
2022-12-17 10:11:52 +0100

    Update Mecanum.h

--- src/Mecanum.h
+++ src/Mecanum.h
@@ -12,7 +12,7 @@ class Mecanum
         Mecanum(uint8_t in1_1, uint8_t in1_2, uint8_t pwm1, uint8_t offset1,
                 uint8_t in2_1, uint8_t in2_2, uint8_t pwm2, uint8_t offset2, uint8_t standBy1,
                 uint8_t in3_1, uint8_t in3_2, uint8_t pwm3, uint8_t offset3,
-                uint8_t in4_1, uint8_t in4_2, uint8_t pwm4, uint8_t offset4, uint8_t standBy2);
+                uint8_t in4_1, uint8_t in4_2, uint8_t pwm4, uint8_t offset4, uint8_t standBy2, int min = 0, int max = 255);
         Motor motors[2][2] = { { Motor(0, 0, 0, 0, 0), Motor(0, 0, 0, 0, 0) }, { Motor(0, 0, 0, 0, 0), Motor(0, 0, 0, 0, 0) } };
         void forward(int speed);
         void backward(int speed);

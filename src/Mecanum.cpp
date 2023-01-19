c418fd0e181948a54f28071179e26e3008f5174c
SimonPucheu <104694344+SimonPucheu@users.noreply.github.com>
2023-01-19 10:46:01 +0100

    Bring back mecaside, diagonal and sideway

--- src/Mecanum.cpp
+++ src/Mecanum.cpp
@@ -5,15 +5,15 @@
 /**
  * Setup the mecanum
  */
-Mecanum::Mecanum(   uint8_t in1_1, uint8_t in1_2, uint8_t pwm1, uint8_t offset1,
-                    uint8_t in2_1, uint8_t in2_2, uint8_t pwm2, uint8_t offset2, uint8_t standBy1,
-                    uint8_t in3_1, uint8_t in3_2, uint8_t pwm3, uint8_t offset3,
-                    uint8_t in4_1, uint8_t in4_2, uint8_t pwm4, uint8_t offset4, uint8_t standBy2, int min = 0, int max = 255)
+Mecanum::Mecanum(uint8_t in1_1, uint8_t in1_2, uint8_t pwm1, uint8_t offset1,
+                 uint8_t in2_1, uint8_t in2_2, uint8_t pwm2, uint8_t offset2, uint8_t standBy1,
+                 uint8_t in3_1, uint8_t in3_2, uint8_t pwm3, uint8_t offset3,
+                 uint8_t in4_1, uint8_t in4_2, uint8_t pwm4, uint8_t offset4, uint8_t standBy2, int min = 0, int max = 255)
 {
-    motors[Left][Top]       = Motor(in1_1, in1_2, pwm1, offset1, standBy1, min, max);
-    motors[Left][Bottom]    = Motor(in2_1, in2_2, pwm2, offset2, standBy1, min, max);
-    motors[Right][Top]      = Motor(in3_1, in3_2, pwm3, offset3, standBy2, min, max);
-    motors[Right][Bottom]   = Motor(in4_1, in4_2, pwm4, offset4, standBy2, min, max);
+    motors[Left][Top] = Motor(in1_1, in1_2, pwm1, offset1, standBy1, min, max);
+    motors[Left][Bottom] = Motor(in2_1, in2_2, pwm2, offset2, standBy1, min, max);
+    motors[Right][Top] = Motor(in3_1, in3_2, pwm3, offset3, standBy2, min, max);
+    motors[Right][Bottom] = Motor(in4_1, in4_2, pwm4, offset4, standBy2, min, max);
 }
 
 void Mecanum::forward(int speed)
@@ -53,5 +53,37 @@ void Mecanum::stop()
     motors[Left][Top].stop();
     motors[Left][Bottom].stop();
     motors[Right][Top].stop();
+    motors[Right][Bottom].stop();
+}
+
+void Mecanum::sidewayLeft(int speed)
+{
+    motors[Left][Top].backward(speed);
+    motors[Left][Bottom].forward(speed);
+    motors[Right][Top].backward(speed);
+    motors[Right][Bottom].forward(speed);
+}
+
+void Mecanum::sidewayRight(int speed)
+{
+    motors[Left][Top].forward(speed);
+    motors[Left][Bottom].backward(speed);
+    motors[Right][Top].forward(speed);
+    motors[Right][Bottom].backward(speed);
+}
+
+void Mecanum::diagonalForward(int speed)
+{
+    motors[Left][Top].forward(speed);
+    motors[Left][Bottom].stop();
+    motors[Right][Top].stop();
+    motors[Right][Bottom].forward(speed);
+}
+
+void Mecanum::diagonalBackward(int speed)
+{
+    motors[Left][Top].stop();
+    motors[Left][Bottom].forward(speed);
+    motors[Right][Top].forward(speed);
     motors[Right][Bottom].stop();
 }
--- src/Mecanum.h
+++ src/Mecanum.h
@@ -8,15 +8,46 @@
 
 class Mecanum
 {
-    public:
-        Mecanum(uint8_t in1_1, uint8_t in1_2, uint8_t pwm1, uint8_t offset1,
-                uint8_t in2_1, uint8_t in2_2, uint8_t pwm2, uint8_t offset2, uint8_t standBy1,
-                uint8_t in3_1, uint8_t in3_2, uint8_t pwm3, uint8_t offset3,
-                uint8_t in4_1, uint8_t in4_2, uint8_t pwm4, uint8_t offset4, uint8_t standBy2, int min = 0, int max = 255);
-        Motor motors[2][2] = { { Motor(0, 0, 0, 0, 0), Motor(0, 0, 0, 0, 0) }, { Motor(0, 0, 0, 0, 0), Motor(0, 0, 0, 0, 0) } };
-        void forward(int speed);
-        void forward(int speed, int duration);
-        void backward(int speed);
-        void backward(int speed, int duration);
-        void stop();
+public:
+	Mecanum(uint8_t in1_1, uint8_t in1_2, uint8_t pwm1, uint8_t offset1,
+			uint8_t in2_1, uint8_t in2_2, uint8_t pwm2, uint8_t offset2, uint8_t standBy1,
+			uint8_t in3_1, uint8_t in3_2, uint8_t pwm3, uint8_t offset3,
+			uint8_t in4_1, uint8_t in4_2, uint8_t pwm4, uint8_t offset4, uint8_t standBy2, int min = 0, int max = 255);
+	Motor motors[2][2] = {{Motor(0, 0, 0, 0, 0), Motor(0, 0, 0, 0, 0)}, {Motor(0, 0, 0, 0, 0), Motor(0, 0, 0, 0, 0)}};
+	void forward(int speed);
+	void forward(int speed, int duration);
+	void backward(int speed);
+	void backward(int speed, int duration);
+	void stop();
+	void sidewayLeft(int speed);
+	void sidewayRight(int speed);
+	void diagonalForward(int speed);
+	void diagonalBackward(int speed);
+};
+
+class Mecaside
+{
+public:
+	Mecaside(int iSide, Mecanum &iMecanum)
+	{
+		side = iSide;
+		mecanum = iMecanum;
+	}
+	Mecanum &mecanum;
+	int side = 0;
+	void forward(int speed)
+	{
+		mecanum.motors[side][0].forward(speed);
+		mecanum.motors[side][1].forward(speed);
+	}
+	void backward(int speed)
+	{
+		mecanum.motors[side][0].backward(speed);
+		mecanum.motors[side][1].backward(speed);
+	}
+	void stop()
+	{
+		mecanum.motors[side][0].stop();
+		mecanum.motors[side][1].stop();
+	}
 };
